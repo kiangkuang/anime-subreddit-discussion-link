5bc74334307b9148f0d0c912158677658e710417
Kiang Kuang <kiangkuang@gmail.com>
2025-05-03 17:22:27 +0800

    feat: bump version to 1.5.0 and add support for hianimez

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,15 @@ import monkey, { MonkeyUserScript } from "vite-plugin-monkey";
 const userscript: MonkeyUserScript = {
   name: "Anime Subreddit Discussion Link",
   namespace: "https://github.com/kiangkuang",
-  version: "1.4.0",
+  version: "1.5.0",
   description:
     "Adds a link to Anime Subreddit episode discussion threads on anime platforms.",
   author: "Kiang Kuang",
-  include: ["*://hianime.tld/watch/*", "https://www.netflix.com/*"],
+  include: [
+    "*://hianime.tld/watch/*",
+    "*://hianimez.tld/watch/*",
+    "https://www.netflix.com/*",
+  ],
   icon: "https://www.google.com/s2/favicons?sz=64&domain=github.com",
 };
 
